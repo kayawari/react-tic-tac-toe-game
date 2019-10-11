06a6e04cd0cb2df1e714200753d4c637f0c9b0ea
takayuki.kayawari <kayamelo@gmail.com>
2019-10-12 00:36:12 +0900

    divide index.html for React element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 import Game from './tic_tac_toe/index';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root1')
+);
 
 ReactDOM.render(
   <Game />,
-  document.getElementById('root')
+  document.getElementById('root2')
 );
